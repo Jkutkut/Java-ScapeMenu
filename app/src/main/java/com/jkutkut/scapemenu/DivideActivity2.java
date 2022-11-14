9ef06971cf2ac99a4c2a628bd3bf237f177da8fb
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-14 17:45:20 +0100

    all activities created

---
+++ app/src/main/java/com/jkutkut/scapemenu/DivideActivity2.java
@@ -0,0 +1,14 @@
+package com.jkutkut.scapemenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DivideActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_divide2);
+    }
+}
---
+++ app/src/main/java/com/jkutkut/scapemenu/KeygenActivity.java
@@ -0,0 +1,14 @@
+package com.jkutkut.scapemenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KeygenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keygen);
+    }
+}
